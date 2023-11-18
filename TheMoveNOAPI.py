3c9ee7f98fc3051e0bc1b2d4e22732cc5cd0f24d
Logan Sowder <142812448+Logantron@users.noreply.github.com>
2023-11-18 17:35:09 -0500

    Add files via upload

---
+++ TheMoveNOAPI.py
@@ -0,0 +1,31 @@
+# import requests module
+import requests 
+
+# Google Maps API key allows communication with server
+api_key = ''
+
+# url base
+url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+# input will be input into the Google Maps seach API 
+query = input('Search query: ') 
+
+
+# build the new url with our search
+#plugging this new url into a web browser shows us all sorts of data about the Google Maps API
+#Like type of establishment, open or closed currently, size of building, rating, etc.
+#also exact location(lat and lon), this will help us rank the results later on
+request_url = requests.get(url + 'query=' + query +
+						'&key=' + api_key) 
+
+# convert into json
+json_url = request_url.json() 
+
+
+results_list = json_url['results'] 
+
+# keep looping upto length of y 
+#max result is 20, length is based on number of results
+for single_result in range(len(results_list)): 
+    # Print the list one result at a time
+	print(results_list[single_result]['name']) 

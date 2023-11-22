d11575ee5e90e30ed717c24a57d74976998ccab2
Logan Sowder <142812448+Logantron@users.noreply.github.com>
2023-11-22 18:18:48 -0500

    Add files via upload

--- TheMoveNOAPI.py
+++ TheMoveNOAPI.py
@@ -1,7 +1,7 @@
 # import requests module
 import requests 
 
-# Google Maps API key allows communication with server
+# Google Maps API key allowsautomated communication with the server
 api_key = ''
 
 # url base
@@ -11,21 +11,40 @@ url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
 query = input('Search query: ') 
 
 
-# build the new url with our search
+# build the new url with our input
 #plugging this new url into a web browser shows us all sorts of data about the Google Maps API
 #Like type of establishment, open or closed currently, size of building, rating, etc.
 #also exact location(lat and lon), this will help us rank the results later on
 request_url = requests.get(url + 'query=' + query +
 						'&key=' + api_key) 
 
+#this makes getting the request url easy, so I can plug it into my broswer for reference
+reference_request_url = url + 'query=' + query + '&key=' + api_key
+
+#printing the url here lets me double check my work on a browser
+print(reference_request_url)
+
 # convert into json
 json_url = request_url.json() 
 
+results_list_pull = []
+save_list = []
 
 results_list = json_url['results'] 
 
-# keep looping upto length of y 
+# keep looping up to length of y
 #max result is 20, length is based on number of results
 for single_result in range(len(results_list)): 
     # Print the list one result at a time
-	print(results_list[single_result]['name']) 
+	print(results_list[single_result]['name'])
+	#along with their types
+	print(results_list[single_result]['types'])
+	#Also create a new list from the results of the search
+	results_list_pull.append(results_list[single_result]['name'])
+
+#ask for which item they would like to save
+save_number =int(input("Enter corresponding number for item you wish to save"))
+#throw it onto a new list
+save_list.append(results_list_pull[save_number])
+#print save list
+print(save_list)
